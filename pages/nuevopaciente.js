354c2978a54010fce7dfe6ffac73608ea6351ea4
Hardash01 <edwin98santos@gmail.com>
2023-06-10 12:23:05 -0600

    avances sabado

--- pages/nuevopaciente.js
+++ pages/nuevopaciente.js
@@ -1,9 +1,222 @@
 import Layout from "../layout/Layout";
 
 export default function Nuevopaciente() {
+
     return(
         <Layout pagina='Nuevo Paciente'>
-            <h1>Desde Nuevo Paciente</h1>
+                {/* Inicia Contenido de registro */}
+            <div className="flex min-h-full flex-1 flex-col justify-center lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm ">
+                    <h2 className=" text-center text-2xl font-bold leading-9 tracking-tight text-black">
+                        Regístro de Nuevo Pacientes
+                    </h2>
+                </div>
+            <div className="bg-white md:p-8 rounded-lg mt-5">
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-dm">
+                    <form className="space-y-6" action="#" method="POST">
+                    <div className="flex gap-2">
+                        <div className="w-full">
+                            <label htmlFor="nombre" className="block text-base font-medium leading-6 text-black">
+                            Nombre
+                            </label>
+                            <div className="mt-2">
+                            <input
+                                id="nombre"
+                                name="nombre"
+                                type="text"
+                                autoComplete="nombre"
+                                required
+                                placeholder="Su nombre"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                            </div>
+                        </div>
+                        <div className="w-full">
+                            <label htmlFor="apellidos" className="block text-base font-medium leading-6 text-black">
+                            Apellidos
+                            </label>
+                            <div className="mt-2">
+                            <input
+                                id="apellidos"
+                                name="apellidos"
+                                type="text"
+                                autoComplete="apellidos"
+                                required
+                                placeholder="Sus apellidos"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="email" className="block text-base font-medium leading-6 text-black">
+                        Correo Electronico
+                        </label>
+                        <div className="mt-2">
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            autoComplete="email"
+                            required
+                            placeholder="Escriba su correo electronico"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        </div>
+                    </div>
+
+                    <div className="flex gap-2">
+                        <div className="w-full">
+                            <label htmlFor="sexo" className="block text-base font-medium leading-6 text-black">
+                            Sexo
+                            </label>
+                            <div className="mt-2">
+                                <select 
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-center"
+                                    id="sexo"
+                                    required
+                                >
+                                    <option value="0">-- Seleccione --</option>
+                                    <option value="masculino">Masculino</option>
+                                    <option value="femenino">Femenino</option>
+                                    <option value="otros">Otros</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="w-full">
+                            <label htmlFor="edad" className="block text-base font-medium leading-6 text-black">
+                            Edad
+                            </label>
+                            <div className="mt-2">
+                            <input
+                                id="edad"
+                                name="edad"
+                                type="number"
+                                autoComplete="edad"
+                                required
+                                placeholder="Escriba su edad"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex gap-2">
+                    <div className="w-full">
+                            <label htmlFor="telefono" className="block text-base font-medium leading-6 text-black">
+                            Telefono
+                            </label>
+                            <div className="mt-2">
+                            <input
+                                id="telefono"
+                                name="telefono"
+                                type="number"
+                                autoComplete="telefono"
+                                required
+                                placeholder="Escriba su Telefono"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                            </div>
+                        </div>
+                        <div className="w-full">
+                            <label htmlFor="departamento" className="block text-base font-medium leading-6 text-black">
+                            Departamento
+                            </label>
+                            <div className="mt-2">
+                                
+                                <select 
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-center"
+                                    id="departamento"
+                                    required
+                                >
+                                    <option value="0">-- Seleccione --</option>
+                                    <option value="ahuachapan">Ahuachapán</option>
+                                    <option value="santaana">Santa Ana</option>
+                                    <option value="sonsonate">Sonsonate</option>
+                                    <option value="lalibertad">La Libertad</option>
+                                    <option value="chalatenango">Chalatenango</option>
+                                    <option value="cuscatlan">Cuscatlán</option>
+                                    <option value="sansalvador">San Salvador</option>
+                                    <option value="lapaz">La Paz</option>
+                                    <option value="cabañas">Cabañas</option>
+                                    <option value="sanvicente">San Vicente</option>
+                                    <option value="usulutan">Usulután</option>
+                                    <option value="sanmiguel">San Miguel</option>
+                                    <option value="morazan">Morazán</option>
+                                    <option value="launion">La Unión</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="comunidad" className="block text-base font-medium leading-6 text-black">
+                        Direccion
+                        </label>
+                        <div className="mt-2">
+                        <input
+                            id="comunidad"
+                            name="comunidad"
+                            type="text"
+                            required
+                            placeholder="Escriba su Direccion"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        </div>
+                    </div>
+        
+                    <div>
+                        <div>
+                        <label htmlFor="password" className="block text-base font-medium leading-6 text-black">
+                            Contraseña
+                        </label>
+                        </div>
+                        <div className="mt-2">
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            autoComplete="current-password"
+                            required
+                            placeholder="Escribe tu Contraseña"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                        <label htmlFor="confirmacion" className="block text-base font-medium leading-6 text-black">
+                            Confirme Contraseña
+                        </label>
+                        </div>
+                        <div className="mt-2">
+                        <input
+                            id="confirmacion"
+                            name="confirmacion"
+                            type="password"
+                            autoComplete="current-password"
+                            required
+                            placeholder="Confirme su Contraseña"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                        </div>
+                    </div>
+        
+                    <div>
+                        <button
+                        type="submit"
+                        className="flex w-full justify-center rounded-md bg-sky-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600 uppercase"
+                        >
+                        Registrar Usuario
+                        </button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+                
+            </div>
+            {/* finaliza contenido de registro */}
         </Layout> 
     )
 }
--- pages/perfil.js
+++ pages/perfil.js
@@ -4,23 +4,20 @@ export default function Perfil() {
     return(
         <Layout pagina='Mi Perfil'>
             
-            <div className="max-w-full md:p-8 p-2 sm:flex sm:space-x-6 bg-white rounded-t-md">
+            <div className="max-w-full md:p-8 p-2 sm:flex sm:space-x-6 bg-white rounded-t-md justify-center">
                 <div className="flex-shrink-0 w-full mb-6 h-44 sm:h-32 sm:w-32 sm:mb-0">
                     <img src="https://source.unsplash.com/100x100/?portrait?1" alt="" className="object-cover object-center w-full h-full rounded dark:bg-gray-500" />
                 </div>
-                <div className="flex flex-col space-y-4">
+                <div className="flex flex-col space-y-4 justify-center">
                     <div>
                         <h2 className="text-2xl font-semibold">Juan Carlos Rodríguez López</h2>
                         <span className="text-lg dark:text-gray-700">Doctor general</span>
-                    </div>
-                    <div className="space-y-1">
-
                     </div>
                 </div>
                 
             </div>
             
-            <div className="md:grid grid-cols-3 bg-white p-8 md:pt-0">
+            <div className="md:grid grid-cols-3 bg-white p-8 md:pt-0 md:pb-2">
                 <div className=" col-span-1  mt-2 md:mt-0">
                     <p className="flex gap-1 items-center font-semibold">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
@@ -43,12 +40,55 @@ export default function Perfil() {
                 </div>
                 <div className="col-span-1 mt-2 md:mt-0">
                     <p className="flex gap-1 items-center font-semibold">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                    </svg>
+                        Direccion:</p>
+                    <p className="flex md:mt-1 gap-1 items-center text-gray-700">
+                        San Salvador, San salvador
+                    </p>
+                </div>
+            </div>
+            <div className="bg-white p-0 md:p-3">
+                <h2 className="text-xl text-center font-semibold mt-0">Contacto de Emergencia</h2>
+            </div>
+            <div className="md:grid grid-cols-3 bg-white p-8 md:pt-0">
+                <div className=" col-span-1  mt-2 md:mt-0">
+                    <p className="flex gap-1 items-center font-semibold">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                        <path strokeLinecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                        Nombre:</p>
+                    <p className="flex md:mt-1 gap-1 items-center text-gray-700">
+                        Laura Valentina González Sánchez
+                    </p>
+                    <p className="flex gap-1 mt-2 items-center font-semibold">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                        </svg>
+                        Parentesco:</p>
+                    <p className="flex md:mt-1 gap-1 items-center text-gray-700">
+                        Esposa
+                    </p>
+                </div>
+                <div className=" col-span-1 mt-2 md:mt-0">
+                    <p className="flex gap-1 items-center font-semibold">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                    </svg>
+                        Telefono:</p>
+                    <p className="flex md:mt-1 gap-1 items-center text-gray-700">
+                        7000 000
+                    </p>
+                </div>
+                <div className="col-span-1 mt-2 md:mt-0">
+                    <p className="flex gap-1 items-center font-semibold">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                         </svg>
                         Direccion:</p>
                     <p className="flex md:mt-1 gap-1 items-center text-gray-700">
-                        San Salvador, San salvador, comunidad de san salvador
+                        San Salvador, San salvador, 
                     </p>
                 </div>
             </div>
