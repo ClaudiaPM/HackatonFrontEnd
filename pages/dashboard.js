37d95f08a24d1ebdc70ad7bdd82c27188508d4f5
Clau_Padilla <claudia.padmag@gmail.com>
2023-06-20 07:06:31 -0600

    add dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,77 @@
+import { REACT_LOADABLE_MANIFEST } from "next/dist/shared/lib/constants";
 import Layout from "../layout/Layout";
+import { Card, Text, Metric, DonutChart, ProgressBar } from "@tremor/react";
+
+
 
 export default function Dashboard() {
     return(
         <Layout>
-            <h1>Desde Layout 123</h1>
+            <h1>Monitoreos Estadisticos</h1>
+            <Card className="max-w-xs mx-auto">
+             <Text>Progreso Sistema JEBCA</Text>
+            <Metric>40%</Metric>
+             <ProgressBar value={40} className="mt-2" />
+            </Card>
         </Layout>
     )
-}
+} 
+
+
+
+
+
+/*
+
+const ills = [
+    {
+      name: "Fiebre",
+      sales: 40,
+    },
+    {
+      name: "Gripe",
+      sales: 60,
+    },
+    {
+      name: "Diabetes",
+      sales: 60,
+    },
+    {
+      name: "Hipertension",
+      sales: 70,
+    },
+    {
+      name: "Asma",
+      sales: 10,
+    },
+    {
+      name: "Dolor de Cabeza",
+      sales: 30,
+    },
+  ];
+  
+ // const valueFormatter = (number: number) => `$ ${Intl.NumberFormat("us").format(number).toString()}`;
+  
+
+
+
+
+export function Dashboard2 () {
+    return(
+    <Layout>
+        <Card className="max-w-lg">
+          <Title>Sales</Title>
+          <DonutChart
+            className="mt-6"
+            data={cities}
+            category="sales"
+            index="name"
+            valueFormatter={valueFormatter}
+            colors={["slate", "violet", "indigo", "rose", "cyan", "amber"]}
+          />
+        </Card>
+    </Layout>
+    )
+ }
+
+ */
